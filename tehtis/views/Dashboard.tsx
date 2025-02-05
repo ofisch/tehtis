4539478c7033b41e62b077a62df376117aa4f327
Onni Fischer <onni.fischer@gmail.com>
2025-02-05 13:56:33 +0200

    Enhance Dashboard styling and layout; update course names and descriptions

--- tehtis/views/Dashboard.tsx
+++ tehtis/views/Dashboard.tsx
@@ -7,21 +7,23 @@ export const Dashboard = () => {
   // mockup-dataa kurssilistaan
   const courses = {
     course1: {
-      name: "Kurssi 1",
+      name: "Menetelmäopinnot, suomen kieli ja viestintä, syksy ",
       description:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque est saepe cum voluptate voluptates fugiat harum omnis fugit dignissimos aperiam? Eos iure quae minima beatae tempore, minus nostrum eum commodi!n",
     },
     course2: {
-      name: "Kurssi 2",
+      name: "Kansantaloustieteen perusteet (uusi versio, käytä ...",
       description: "Course description",
     },
     course3: {
-      name: "Kurssi 3",
-      description: "Course description",
+      name: "Elements of AI",
+      description:
+        "The Elements of AI koostuu ilmaisista verkkokursseista, jotka ovat tehneet MinnaLearn ja Helsingin yliopisto yhteistyössä. Tavoitteenamme on rohkaista ihan jokaista ikään ja taustaan katsomatta oppimaan tekoälyn perusteet: mitä tekoäly on, mitä sillä voi (ja ei voi) tehdä ja miten luoda tekoälymenetelmiä. Kurssit koostuvat teoriaosuuksista ja käytännön harjoituksista, ja voit suorittaa ne omaan tahtiisi.",
     },
     course4: {
-      name: "Kurssi 4",
-      description: "Course description",
+      name: "Syväsukellus moderniin websovelluskehitykseen",
+      description:
+        "Ota haltuusi React, Redux, Node.js, MongoDB, GraphQL ja TypeScript! Kurssilla tutustutaan JavaScriptilla tapahtuvaan moderniin websovelluskehitykseen. Pääpaino on React-kirjaston avulla toteutettavissa single page -sovelluksissa, ja niitä tukevissa Node.js:llä toteutetuissa REST-rajapinnoissa.",
     },
   };
 
